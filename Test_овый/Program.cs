2e79fadbdb01ff9a737882e61a92a28b2480d476
Goskilz <72606466+Goskilz@users.noreply.github.com>
2020-10-09 16:49:45 +0500

    Initial commit

---
+++ Test_овый/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Test_овый
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x;
+            Console.WriteLine("Enter value");
+            string input = Console.ReadLine();
+            Console.WriteLine(Int32.TryParse(input, out x));
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
